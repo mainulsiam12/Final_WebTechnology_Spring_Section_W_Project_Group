cfd7187c86cc3e55ea31b9ba096c790ffecb648c
arifsaikat01 <arifsaikat230@gmail.com>
2026-05-19 01:03:12 +0600

    addddd

--- api/availability.php
+++ api/availability.php
@@ -2,33 +2,32 @@
 
 header('Content-Type: application/json');
 
-include '../../config/database.php';
+include '../config/database.php';
+
+$database = new Database();
+$conn = $database->OpenCon();
 
 $book_id = $_GET['book_id'];
 
 $sql = "
-SELECT books.total_copies,
-COUNT(CASE WHEN borrow_records.status='Active' THEN 1 END) AS active_count
+SELECT books.total_copies -
+COUNT(CASE WHEN borrow_records.status='Active' THEN 1 END)
+AS available
 
 FROM books
 
 LEFT JOIN borrow_records
 ON books.id = borrow_records.book_id
 
-WHERE books.id=?
+WHERE books.id='$book_id'
 
 GROUP BY books.id
 ";
 
-$stmt = $conn->prepare($sql);
+$result = mysqli_query($conn, $sql);
 
-$stmt->execute([$book_id]);
+$data = mysqli_fetch_assoc($result);
 
-$data = $stmt->fetch(PDO::FETCH_ASSOC);
+echo json_encode($data);
 
-$available = $data['total_copies'] - $data['active_count'];
-
-echo json_encode([
-    'available_copies' => $available
-]);
 ?>
